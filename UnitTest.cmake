4ca07fa25da5138a5056a4cebf9fb794f824af82
Alex <alexkrieg@gmx.ch>
2024-03-18 18:19:05 +0100

    -

--- UnitTest.cmake
+++ UnitTest.cmake
@@ -1,5 +1,4 @@
 ## description: simple library create unit tests
-cmake_minimum_required(VERSION 3.1.0)
 include(FetchContent)
 
 # Define the git repository and tag to download from
@@ -17,7 +16,6 @@ set(${LIB_NAME}_NO_EXAMPLES True)						# Disables the examlpes of the library
 set(${LIB_NAME}_NO_UNITTESTS True)						# Disables the unittests of the library
 message("Downloading dependency: ${LIB_NAME} from: ${GIT_REPO} tag: ${GIT_TAG}")
 FetchContent_MakeAvailable(${LIB_NAME})
-
 
 # Add this library to the specific profiles of this project
 list(APPEND DEPENDENCIES_FOR_SHARED_LIB ${LIB_NAME}_static)
