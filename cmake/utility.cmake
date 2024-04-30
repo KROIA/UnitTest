525b2fb22fcb614abca842fe3e074d1ae6a51ab7
Alex Krieg <alexkrieg@gmx.ch>
2024-04-30 15:35:09 +0200

    ~ QT deployment

--- cmake/utility.cmake
+++ cmake/utility.cmake
@@ -24,45 +24,39 @@ endfunction()
 # Params: targetName        Name of the target created using add_executable(...)
 #         outputPath        Path where the deployment will be done
 #     
- function(DEPLOY_QT targetName outputPath)
+function(DEPLOY_QT targetName outputPath)
     
- # check if QT_PATH is empty
+    # check if QT_PATH is empty
     if (NOT QT_PATH)
 		message("QT_PATH is not set. include QtLocator.cmake first, to find a qt installation or assign a 
                  QT pat to it. example: set(QT_PATH \"C:/Qt/5.14.2\")")
         return()
     endif()
 
-    set(DEPLOY_COMMAND  "${QT_PATH}/bin/windeployqt.exe"
-                --no-compiler-runtime
-                --no-translations
-                --no-system-d3d-compiler
-                --no-opengl-sw
-                --pdb)
+	
 
-
-   set(targetExePath "$<TARGET_FILE_DIR:${targetName}>/$<TARGET_FILE_NAME:${targetName}>")
-
-   string(MD5 TARGETPATH_HASH ${outputPath})
-   set(UNIQUE_TARGET_NAME "deploy_${TARGETPATH_HASH}_${targetName}")
-   message("DeploymentTargetName: ${UNIQUE_TARGET_NAME}")
-
-   if(TARGET ${UNIQUE_TARGET_NAME})
-	   message("Target ${UNIQUE_TARGET_NAME} already exists")
-	   return()
-   endif()
-   add_custom_target(${UNIQUE_TARGET_NAME} ALL
-       DEPENDS "${targetExePath}"
-   )
-    # Deploy easy_profiler gui to bin folder
-     add_custom_command(TARGET ${UNIQUE_TARGET_NAME}
-         COMMAND ${DEPLOY_COMMAND} 
-             --dir "${outputPath}"
-            "${targetExePath}"
- 	    COMMENT "Running windeployqt..." "${QT_PATH}/bin/windeployqt.exe" "${outputPath}"
-        DEPENDS UNIQUE_TARGET_NAME
-     )
- endfunction()
+    string(MD5 TARGETPATH_HASH ${outputPath})
+    set(UNIQUE_TARGET_NAME "deploy_${TARGETPATH_HASH}_${targetName}")
+    message("DeploymentTargetName: ${UNIQUE_TARGET_NAME}")
+ 
+    if(TARGET ${UNIQUE_TARGET_NAME})
+        message("Target ${UNIQUE_TARGET_NAME} already exists")
+        return()
+    endif()
+    add_custom_target(${UNIQUE_TARGET_NAME} ALL
+        DEPENDS "${targetName}"
+    ) 
+	 
+	# Define the custom command to be executed during installation
+    set(targetExePath "${INSTALL_BIN_PATH}/${targetName}.exe") 
+    set(DEPLOY_COMMAND  ${QT_PATH}/bin/windeployqt.exe --no-compiler-runtime --no-translations --no-system-d3d-compiler --no-opengl-sw --pdb --dir "${INSTALL_BIN_PATH}" "${targetExePath}")
+	string(REPLACE "\\" "/" DEPLOY_COMMAND "${DEPLOY_COMMAND}")
+	add_custom_command(
+		TARGET "${UNIQUE_TARGET_NAME}"  # Assuming my_executable is the target to be installed
+		POST_BUILD            # Specify that the command should be executed after building the target
+		COMMAND ${DEPLOY_COMMAND} 
+	)
+endfunction()
 
 
 function(copyLibraryHeaders headerRootFolder destinationPath destinationFolderName)
