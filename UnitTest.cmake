4422e0253c9ca521c77c9a0a272e6e2c634058a3
Alex <alexkrieg@gmx.ch>
2024-09-13 14:06:25 +0200

    -

--- UnitTest.cmake
+++ UnitTest.cmake
@@ -1,4 +1,5 @@
 ## description: simple library create unit tests
+include(FetchContent)
 
 function(dep SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
     # Define the git repository and tag to download from
