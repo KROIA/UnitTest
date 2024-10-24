f579007abf2baa9237289c074985cc7ad262ed1d
Alex Krieg <alexkrieg@gmx.ch>
2024-10-24 11:12:49 +0200

    -

--- UnitTest.cmake
+++ UnitTest.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
     # Define the git repository and tag to download from
     set(LIB_NAME UnitTest)		
-    set(LIB_MACRO_NAME UNIT_TEST)
+    set(LIB_MACRO_NAME UNIT_TEST_LIBRARY_AVAILABLE)
     set(GIT_REPO https://github.com/KROIA/UnitTest.git)	
     set(GIT_TAG main)									
 
